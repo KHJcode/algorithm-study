d69a3623b43401d244e6fdfe2f7795670bf92a5e
KHJcode <kimstudswcode4485@gmail.com>
2020-03-23 02:55:51 +0900

    '200323

---
+++ 5543.py
@@ -0,0 +1,13 @@
+drink = burger = 2001
+
+for i in range(3):
+      n = int(input())
+      burger = min(burger,n)
+
+for j in range(2):
+      n = int(input())
+      drink = min(drink,n)
+
+print(burger+drink-50)
+
+      

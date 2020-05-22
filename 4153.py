ea59c21cce59881f46040258f0255334a3598f71
KHJcode <kimstudswcode4485@gmail.com>
2020-05-23 02:20:35 +0900

    200523

--- 4153.py
+++ 4153.py
@@ -1,6 +1,6 @@
 while 1:
     a,b,c = map(int,input().split())
     if a+b+c == 0:   break
-    a*=a;b*=b;c*=c;
+    a*=a;b*=b;c*=c
     if a+b==c or a+c==b or c+b==a:    print('right')
     else:   print('wrong')

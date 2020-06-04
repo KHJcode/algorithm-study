d01aa305d41ec5c39593845e10931634dc48c38f
= <kimstudswcode4485@gmail.com>
2020-06-04 17:29:23 +0900

    200604

--- 9095.py
+++ 9095.py
@@ -1,3 +1,3 @@
-dp = [1,2,4]
-for i in range(4,11):    dp.append(sum(dp[-3:]))
-for i in range(int(input())):   print(dp[int(input())-1])
+dp = [0,1,2,4]
+for _ in range(4,14):   dp.append(sum(dp[-3:]))
+for _ in range(int(input())):    print(dp[int(input())])

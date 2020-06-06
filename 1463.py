f8ab1dc6de68edd9cd03476c0eb693485e3312db
= <kimstudswcode4485@gmail.com>
2020-06-07 01:30:11 +0900

    200607

--- 1463.py
+++ 1463.py
@@ -1,6 +1,6 @@
-n = int(input());dp = [0,0,1,1]
-for i in range(4,n+1):
+N = int(input());dp = [0,0,1,1]
+for i in range(4,N+1):
     dp.append(dp[i-1]+1)
     if i%2 == 0:    dp[i] = min(dp[i//2]+1, dp[i])
     if i%3 == 0:    dp[i] = min(dp[i//3]+1, dp[i])
-print(dp)
+print(dp[N])
--- 9095.py
+++ 9095.py
@@ -1,3 +1,3 @@
 dp = [0,1,2,4]
-for _ in range(4,14):   dp.append(sum(dp[-3:]))
+for _ in range(4,11):   dp.append(sum(dp[-3:]))
 for _ in range(int(input())):    print(dp[int(input())])

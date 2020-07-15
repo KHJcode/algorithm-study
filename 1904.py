286b7a82fad5c52545e8452473a149f4f4cc9d06
KHJcode <kimstudswcode4485@gmail.com>
2020-07-16 01:14:52 +0900

    200716

--- 1904.py
+++ 1904.py
@@ -1,8 +1,4 @@
 n = int(input())
 dp = [1,2]
-for i in range(2,n):  dp.append((dp[i-1]+dp[i-2])%15746)
+for i in range(2,n):  dp.append(sum(dp[-2:i])%15746)
 print(dp[n-1])
-
-#a=b=1
-#for i in[0]*int(input()):  a,b=b%15746,a+b;print(a,b)
-#print(a,b)

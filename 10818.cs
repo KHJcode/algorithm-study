a862dc8f2a48b8f737d2afebc28fb6b59de3cf1d
KHJcode <kimstudswcode4485@gmail.com>
2020-07-21 03:15:08 +0900

    200721

--- 10818.cs
+++ 10818.cs
@@ -3,17 +3,16 @@ public class Program {
   public static void Main()
   {
     int n = int.Parse(Console.ReadLine());
-    string[] input = Console.ReadLine().Split(' ');
-    int[] numbers = new int[n];
-    for (int i = 0; i < n; i++) numbers[i] = int.Parse(input[i]);
+    string[] input = Console.ReadLine().Split();
+    int[] numbers = Array.ConvertAll(input, int.Parse);
 
-    int max = -1000001;
-    int min = 1000001;
+    int max = numbers[0];
+    int min = numbers[0];
     foreach (int number in numbers) {
       if (max < number) max = number;
       else if (min > number) min = number;
     }
 
-    Console.WriteLine($"{min} {max}");
+    Console.WriteLine(min+" "+max);
   }
 }

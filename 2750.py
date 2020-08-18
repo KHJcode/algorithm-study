4cf187d63bd3041280f24afadfcb9ccc0f08b20d
KHJcode <kimstudswcode4485@gmail.com>
2020-08-19 06:31:45 +0900

    solve problem

--- 2750.py
+++ 2750.py
@@ -1,3 +1,13 @@
-n = [int(input()) for _ in range(int(input()))]
-n.sort()
-for i in n:  print(i)
+import sys
+
+def bubbleSort(array):
+  for i in range(len(array) - 1):
+    for j in range(i + 1, len(array)):
+      if array[i] > array[j]:  array[i], array[j] = array[j], array[i]
+  return array
+
+n = int(sys.stdin.readline())
+numbers = [int(sys.stdin.readline()) for _ in range(n)]
+
+result = bubbleSort(numbers)
+for i in result:  print(i)

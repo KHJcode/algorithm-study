d0ae522281e441d90e062bd798bb52efae45f41b
KHJcode <kimstudswcode4485@gmail.com>
2020-11-18 01:51:24 +0900

    recursion algorithm

--- 10870.py
+++ 10870.py
@@ -1,4 +1,8 @@
-def fibonacci(num):
-  if num >= 2:  return fibonacci(num-1)+fibonacci(num-2)
-  return num;
-print(fibonacci(int(input())))
+def getFibonacci(n):
+  if n <= 1:  return n
+  return getFibonacci(n - 1) + getFibonacci(n - 2)
+
+data = int(input())
+
+result = getFibonacci(data)
+print(result)
--- 2748.py
+++ 2748.py
@@ -1,4 +1,9 @@
-n = int(input())
-dp = [0,1]
-for i in range(2,n+1):  dp.append(dp[i-1]+dp[i-2])
-print(dp[n])
+def getFibonacci(n):
+  dp = [0, 1]
+  for i in range(2, n + 1):  dp.append(dp[i - 1] + dp[i - 2])
+  return dp[n]
+
+data = int(input())
+
+result = getFibonacci(data)
+print(result)

15714b974d9b0b9776023b74448c10482262079d
KHJcode <kimstudswcode4485@gmail.com>
2020-11-28 00:37:20 +0900

    4344 update

--- 4344.py
+++ 4344.py
@@ -1,9 +1,17 @@
+def getAverageOverPercentage(n, score):
+  avg = sum(score)/n
+  std = 0
+
+  for i in score:
+    if i > avg:  std += 1
+
+  return round(std/n*100, 3)
+
+
+
 n = int(input())
-for i in range(n):
-      score = list(map(int,input().split()))
-      avg = sum(score[1:])/score[0]
-      add = 0
-      for j in score[1:]:
-            if j > avg:
-                  add += 1
-      print("%.3f"%round((add/score[0])*100, 3)+"%")
+
+for _ in range(n):
+  data = list(map(int, input().split()))
+  result = getAverageOverPercentage(data[0], data[1:])
+  print("%.3f"%result + '%')

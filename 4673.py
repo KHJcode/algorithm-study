4acd73a62232bfe2ad0fd2c7cf8592644a570dd6
KHJcode <kimstudswcode4485@gmail.com>
2020-12-01 23:02:12 +0900

    4673 update

--- 4673.py
+++ 4673.py
@@ -1,1 +1,13 @@
+def d(n):
+  num = 0
+  for i in str(n):  num += int(i)
+  return n + num
 
+def getSelfNumber():
+  notSelfNumber = []
+  for i in range(0, 10001):  notSelfNumber.append(d(i))
+  for j in range(0, 10001):
+    if j in notSelfNumber:  continue
+    print(j)
+
+getSelfNumber()

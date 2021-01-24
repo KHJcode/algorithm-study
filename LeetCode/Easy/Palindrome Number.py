afce37799089a350ac864cfb8e4e28f7fedd0c1f
KHJcode <kimstudswcode4485@gmail.com>
2021-01-24 11:15:45 +0900

    palindrome number

---
+++ LeetCode/Easy/Palindrome Number.py
@@ -0,0 +1,1 @@
+def isPalindrome(self, x):  return str(x)[::-1] == str(x) and x >= 0
--- LeetCode/Easy/Reverse Integer.py
+++ LeetCode/Easy/Reverse Integer.py
@@ -1,3 +1,3 @@
 def reverse(self, x):
-    _x = int(str(x)[::-1] if 0 <= x else '-' + str(x)[1:][::-1])
-    return _x if -2**31 < _x < 2**31-1 else 0
+  _x = int(str(x)[::-1] if 0 <= x else '-' + str(x)[1:][::-1])
+  return _x if -2**31 < _x < 2**31-1 else 0

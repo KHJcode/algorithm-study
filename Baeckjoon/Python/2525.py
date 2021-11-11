5b29cbf3a5f49279fec7ee2ab1f486373d402afa
KHJcode <kimstudswcode4485@gmail.com>
2021-11-11 19:15:00 +0900

    today solve

---
+++ Baeckjoon/Python/2525.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+c = int(input())
+a += c // 60
+b += c % 60
+if b > 59:
+  a += b // 60
+  b -= 60
+if a > 23:
+  a -= 24
+print(a, b)
---
+++ Baeckjoon/Python/2756.py
@@ -0,0 +1,2 @@
+numbers = list(map(int, input().split()))
+print(' '.join(str(n) for n in sorted(numbers)))

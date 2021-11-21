4a9ef128f00a8cd51699452c3709a4ef2ec232d8
KHJcode <kimstudswcode4485@gmail.com>
2021-11-21 23:51:12 +0900

    today ps

---
+++ Baeckjoon/C/21300.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c, d, e, f;
+
+  scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+  printf("%d", a * 5 + b * 5 + c * 5 + d * 5 + e * 5 + f * 5);
+
+  return 0;
+}

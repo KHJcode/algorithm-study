02591c0120478f5c79348903b7073e26732b0acf
Hyungjin Kim <to@khjcode.com>
2023-04-09 11:38:03 +0900

    Create 15971.cpp

---
+++ Baekjoon/Cpp/15971.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define MAX 100001
+
+using namespace std;
+
+vector<pair<int, int>> va[MAX];
+
+int b, res, gph[MAX];
+
+void rec(int x, int s, int mx) {
+    int v, i;
+    if (gph[x] == 0 && res == 0) {
+        gph[x] = 1;
+        if (x == b) {
+            res = s - mx;
+        } else {
+            for (auto i : va[x]) {
+                rec(i.first, s + i.second, mx > i.second ? mx : i.second);
+            }
+        }
+    }
+}
+
+int main() {
+    int a, n, i, j, k, d;
+    cin >> n >> a >> b;
+    for (i = 0; i < n - 1; i++) {
+        cin >> j >> k >> d;
+        va[j].push_back({k, d});
+        va[k].push_back({j, d});
+    }
+    rec(a, 0, 0);
+    cout << res;
+    return 0;
+}
